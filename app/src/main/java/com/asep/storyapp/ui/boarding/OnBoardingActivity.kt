6422afc0c145e06b5d31c259a1a7a9da5c530543
Asep Sutisna <sutisnaasep323@gmail.com>
2022-11-11 19:07:58 +0700

    onProgres_3_onBoarding

---
+++ app/src/main/java/com/asep/storyapp/ui/boarding/OnBoardingActivity.kt
@@ -0,0 +1,25 @@
+package com.asep.storyapp.ui.boarding
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.asep.storyapp.R
+import com.asep.storyapp.databinding.ActivityOnBoardingBinding
+import com.asep.storyapp.ui.MainActivity
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnBoardingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
